a5ec9113639b5f3bb9c426bd3924e2eb0f1a2e7a
cparradoc <cparradoc@minsait.com>
2022-03-18 17:22:44 +0100

    object type properties

---
+++ Curso JS Moderno/07-Objetos/js/05-app.js
@@ -0,0 +1,24 @@
+const Producto2 = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+    informacion: {
+        medidas: {
+            peso: '1kg',
+            medida: '1m'
+        },
+        fabricacion: {
+            pais: 'China'
+        }
+    }
+    
+};
+
+
+console.log(Producto2);
+
+console.log(Producto2.informacion);
+
+console.log(Producto2.informacion.medida);
+
+console.log(Producto2.informacion.fabricacion.pais);
