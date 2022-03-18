3eccce130a637529e2388be42577aca2433642e1
cparradoc <cparradoc@minsait.com>
2022-03-18 14:21:57 +0100

    math ops

---
+++ Curso JS Moderno/04-Numeros/js/03-app.js
@@ -0,0 +1,40 @@
+const numero8 = 30;
+const numero9 = 20;
+
+let resultado2;
+
+//PI
+resultado2 = Math.PI;
+
+//Redondeo
+resultado2 = Math.round(Math.PI);
+resultado2 = Math.round(2.51);
+
+//Redondeo hacia arriba
+resultado2 = Math.ceil(2.1);
+
+//Redondeo hacia abajo
+resultado2 = Math.floor(2.9);
+
+//Raiz cuadrada
+resultado2 = Math.sqrt(144);
+
+//Valor absoluto
+resultado2 = Math.abs(-150);
+
+//Potencia
+resultado2 = Math.pow(2, 4);
+
+//Minimo
+resultado2 = Math.min(2, 3, 4, 12, 1);
+
+//Maximo
+resultado2 = Math.max(2, 3, 4, 12, 1);
+
+//Aleatorio entre 0 y 1
+resultado2 = Math.random();
+
+//Aleatorio en un rango
+resultado2 = Math.floor(Math.random() * 30);
+
+console.log(resultado2);
