3035510fc774221996d70e72082e44bc72c11527
cparradoc <cparradoc@minsait.com>
2022-03-18 14:03:20 +0100

    basic operations

---
+++ Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,21 @@
+const numero01 = 30;
+const numero02 = 20;
+
+let resultado;
+
+//Suma
+resultado = numero01 + numero02;
+
+//Resta
+resultado = numero01 - numero02;
+
+//Multiplicación
+resultado = numero01 * numero02;
+
+//División
+resultado = numero01 / numero02;
+
+//Módulo
+resultado = numero01 % numero02;
+
+console.log(resultado);
