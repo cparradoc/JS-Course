7981dd2779ad26147322eca0eab5d94308315354
cparradoc <cparradoc@minsait.com>
2022-03-18 14:30:16 +0100

    x++ ++x

---
+++ Curso JS Moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,16 @@
+let puntaje = 5;
+
+//Se añade después de imprimirlo
+console.log(puntaje++);
+
+//Se añade antes de imprimirlo
+console.log(++puntaje);
+
+
+console.log(puntaje);
+
+//Sumar de dos en dos
+let puntaje2 = 5;
+puntaje2 += 2;
+
+console.log(puntaje2);
