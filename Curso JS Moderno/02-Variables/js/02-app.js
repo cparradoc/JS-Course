6feebd21859e1049032ee45f364e7f941925adf1
cparradoc <cparradoc@minsait.com>
2022-03-18 13:08:48 +0100

    let and const

---
+++ Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,10 @@
+let producto2 = "Tablet";
+
+//Reasignar valor
+
+producto2 = "Monitor";
+
+producto2 = 20;
+
+//Puede no asignarse un valor, sería un indefinido
+let precio;
---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,7 @@
+//Necesita inicializarse
+const producto3 = "pomelo";
+
+//No puede ser reasignado
+producto3 = "patata";
+console.log(producto3);
+
