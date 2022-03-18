ae9ade46b7c47ad1efe2a6c5483dffa904273de7
cparradoc <cparradoc@minsait.com>
2022-03-18 13:44:17 +0100

    touppercase, tolowercase, tostring

---
+++ Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,12 @@
+const monitor5 = "Monitor de 20 pulgadas";
+
+//Pasar string a mayúsculas
+console.log(monitor5.toUpperCase());
+
+//Pasar string a minúsculas
+console.log(monitor5.toLowerCase());
+
+const precio2 = 300;
+console.log(precio2);
+console.log(precio2.toString());
+
