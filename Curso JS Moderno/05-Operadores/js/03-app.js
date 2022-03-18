6fc9c700cfb4e9b6bf31e99f4f7f6dd94670679f
cparradoc <cparradoc@minsait.com>
2022-03-18 14:58:31 +0100

    null and undefined

---
+++ Curso JS Moderno/05-Operadores/js/03-app.js
@@ -0,0 +1,12 @@
+//Undefined
+let numero;
+console.log(numero);
+console.log(typeof numero);
+
+//Null
+let numeroo = null;
+console.log(numeroo);
+console.log(typeof numeroo);
+
+console.log(numero == numeroo);
+console.log(numero === numeroo);
