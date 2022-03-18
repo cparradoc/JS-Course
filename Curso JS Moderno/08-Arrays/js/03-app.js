7594797dc5d559b7559ee58b7befe3e3dd6d3801
cparradoc <cparradoc@minsait.com>
2022-03-18 19:10:18 +0100

    iterate array

---
+++ Curso JS Moderno/08-Arrays/js/03-app.js
@@ -0,0 +1,9 @@
+const meses2 = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio']
+
+console.table(meses2);
+
+console.log(meses2.length);
+
+for(let i = 0; i < meses2.length; i++) {
+    console.log(meses2[i]);
+}
