3f8157858f31a7b90b9589d0817028bb5c2efcd6
cparradoc <cparradoc@minsait.com>
2022-03-18 12:09:56 +0100

    basic comments

--- Curso JS Moderno/01-Introduccion/scripts/app.js
+++ Curso JS Moderno/01-Introduccion/scripts/app.js
@@ -1,2 +1,7 @@
 const nombre = prompt('Cuál es tu nombre?');
+
+//Toma el nombre y lo muestra en pantalla
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno`;
+
+//Asignamos el valor de la variable producto
+const producto = 'Monitor de 24 pulgadas';
