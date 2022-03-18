3b7962908f3037e23e75f52017655e1782829e64
cparradoc <cparradoc@minsait.com>
2022-03-18 19:02:13 +0100

    array declaration

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,10 @@
+const numeros = [10, 20, 30];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+
+console.log(numeros);
+console.log(meses);
+
+const deTodo = [10, "patata", 2.5, true, null, {nombre: 'Carlos', trabajo: 'Frontend developer'}, [1, 2, 3]];
+console.log(deTodo);
