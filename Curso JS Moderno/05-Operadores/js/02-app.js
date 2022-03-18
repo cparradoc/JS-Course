8fc579185f6fbbe0d87b428752655f4560a8f871
cparradoc <cparradoc@minsait.com>
2022-03-18 14:54:10 +0100

    comparing operators

---
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -0,0 +1,25 @@
+
+const numero4 = 20;
+const numero5 = "20";
+const numero6 = 30;
+
+//Revisaro si son iguales
+console.log(numero1 == numero3);
+console.log(numero1 == numero2);
+
+//Comparador estricto, compara valor y tipo de dato
+console.log(numero1 === numero2);
+console.log(numero1 === parseInt(numero2));
+console.log(numero1.toString() === numero2);
+
+console.log(typeof numero4);
+console.log(typeof numero5);
+
+//Comparar si son diferentes
+const password1 = "admin";
+const password2 = "Admin";
+
+console.log(password1 != password2);
+console.log(numero4 != numero5);
+console.log(numero4 !== numero5);
+
