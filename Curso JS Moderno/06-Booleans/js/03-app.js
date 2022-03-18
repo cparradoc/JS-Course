3608ea41cc6a80fd7dd02a143e901b480e809598
cparradoc <cparradoc@minsait.com>
2022-03-18 15:25:39 +0100

    ternary operator

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = true;
+
+
+if (autenticado) {
+    console.log("Sí, puedes ver netflix");
+}else {
+    console.log("No, no puedes verlo");
+}
+
+//Operador ternario
+console.log(autenticado ? "Sí, puedes ver netflix" : "No, no puedes verlo");
