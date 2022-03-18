cf606b94c437c8b26642fdac3bb0e8c580a87e5e
cparradoc <cparradoc@minsait.com>
2022-03-18 12:56:23 +0100

    variable naming

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,27 @@
+//js no requiere tipado, para eso utilizar ts
+var producto = "Monitor de 24 pulgadas";
+
+//Reasignación de valores
+producto = "Monitor de 19 pulgadas";
+
+//Js es un lenguaje de tipo dinámico, no se especifica el tipo de dato
+producto = 20;
+
+//Se puede inicializar sin valor y asignarlo después
+var disponible;
+
+disponible = true;
+
+//Inicializar múltiples variables
+var categoria = "Computadoras",
+ marca = "Marca Famosa",
+ calificación = 5;
+
+//Los nombres de variable no pueden comenzar por un número
+
+//Camel Case
+var nombreProducto;
+//Underscore
+var nombre_producto;
+//Pascal Case
+var Producto;
