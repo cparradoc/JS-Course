5a01b8bd399003db4c1724c15949fa1a1d0addec
cparradoc <cparradoc@minsait.com>
2022-03-19 16:11:59 +0100

    < > comparisons

---
+++ Curso JS Moderno/10-Estructuras/js/03-app.js
@@ -0,0 +1,18 @@
+//Operador mayor y menor que
+
+const dinero = 100;
+const totalAPagar = 300;
+
+if( dinero > totalAPagar) {
+    console.log("Podemos pagar");
+} else {
+    console.log("No podemos pagar");
+}
+
+const dinero2 = 301;
+
+if( totalAPagar < dinero2) {
+    console.log("Podemos pagar");
+} else {
+    console.log("No podemos pagar");
+}
