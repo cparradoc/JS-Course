4a951376b97d51ba0ac26534006f6fc55bd13f0b
cparradoc <cparradoc@minsait.com>
2022-03-19 15:24:28 +0100

    arrow functions

---
+++ Curso JS Moderno/09-Funciones/js/10-app.js
@@ -0,0 +1,15 @@
+const aprendiendo = function () {
+    console.log("Aprendiendo JavaScript");
+}
+
+const aprendiendo2 = () => {
+    console.log("Aprendiendo JavaScript");
+};
+
+const aprendiendo3 = () =>  console.log("Aprendiendo JavaScript");
+
+aprendiendo();
+
+aprendiendo2();
+
+aprendiendo3();
