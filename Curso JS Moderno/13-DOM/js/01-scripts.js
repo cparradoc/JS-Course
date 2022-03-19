d2ce82e254b57e6e37b1b36b67f3159aac42c303
cparradoc <cparradoc@minsait.com>
2022-03-19 20:03:52 +0100

    html elements accessing

---
+++ Curso JS Moderno/13-DOM/js/01-scripts.js
@@ -0,0 +1,18 @@
+let elemento;
+
+elemento = document;
+elemento = document.all; //todas las etiquetas
+elemento = document.head;
+elemento = document.domain; //en qué url se carga el documento
+elemento = document.forms; //formularios del documento (array)
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+elemento = document.links; //todos los links del documento (array)
+elemento = document.images;
+elemento = document.scripts;
+
+
+
+
+console.log(elemento);
