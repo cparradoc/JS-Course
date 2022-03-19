0c8c2e344c35afcccb7840249ba101469c4c37ed
cparradoc <cparradoc@minsait.com>
2022-03-19 14:59:06 +0100

    function parameters

---
+++ Curso JS Moderno/09-Funciones/js/05-app.js
@@ -0,0 +1,13 @@
+function sumar(a, b) {
+    console.log(a + b);
+};
+
+sumar(2, 3);
+sumar(2.5, -900);
+
+function saludar(nombre, apellido) {
+    console.log(`Hola ${nombre} ${apellido}`);
+};
+
+saludar("Carlos", "Parrado");
+saludar();
