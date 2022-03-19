8a85db208f7006e1d76c3ce5c431b13b8770f766
cparradoc <cparradoc@minsait.com>
2022-03-19 16:08:49 +0100

    strict comparer

---
+++ Curso JS Moderno/10-Estructuras/js/02-app.js
@@ -0,0 +1,13 @@
+const puntaje = 1000;
+
+if(puntaje === "1000") {
+    console.log("Si es igual");
+} else {
+    console.log("No es igual");
+}
+
+if(puntaje == "1000") {
+    console.log("Si es igual");
+} else {
+    console.log("No es igual");
+}
