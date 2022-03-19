acfc65883dc78db453a572bb72a4f317ead2d21b
cparradoc <cparradoc@minsait.com>
2022-03-19 19:09:19 +0100

    .some

--- Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -7,4 +7,31 @@ const carrito = [
     { nombre: 'Audifonos', precio: 300 },
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
-]
+];
+
+
+//Existe un valor en un array
+meses.forEach( mes => {
+    if (mes == 'Enero') {
+        console.log('Enero si existe');
+    }
+});
+
+const resultado = meses.includes('Enero');
+console.log(resultado);
+
+//Existe un valor en un objeto
+
+const existe = carrito.some( producto => {
+    return producto.nombre === 'Celular';
+});
+
+const existe2 = carrito.some( producto => producto.nombre === 'Febrero');
+
+console.log(existe2);
+
+//.some en un array normal
+
+const existe3 = meses.some( mes => mes === 'Febrero');
+console.log(existe3);
+
