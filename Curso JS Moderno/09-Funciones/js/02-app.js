d3007685905424de8eceba6c2c654294422bac0a
cparradoc <cparradoc@minsait.com>
2022-03-19 14:40:50 +0100

    function expression and declaration differences

---
+++ Curso JS Moderno/09-Funciones/js/02-app.js
@@ -0,0 +1,14 @@
+//Declaración de funciones
+//Esta función funciona
+sumar();
+function sumar() {
+    console.log(2 + 2);
+};
+
+
+//Expresión de función
+//Esta no
+sumar2();
+const sumar2 = function() {
+    console.log(3+3);
+}
