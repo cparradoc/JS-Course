91213189e0c39acaa4bf2f1903de1a2c330f42f6
cparradoc <cparradoc@minsait.com>
2022-03-19 18:35:59 +0100

    do while

---
+++ Curso JS Moderno/11-Iteradores/js/05-app.js
@@ -0,0 +1,15 @@
+i=0;
+
+do {
+
+    console.log(i);
+    i++;
+}while(i < 10);
+
+//se ejecuta una primera vez y luego comprueba si se puede o no
+j = 1000;
+do {
+
+    console.log(j);
+    j++;
+}while(j < 10);
