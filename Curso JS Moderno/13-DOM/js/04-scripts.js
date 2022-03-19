a690b9b21fecc2d61dbcc52481da920aaee06f68
cparradoc <cparradoc@minsait.com>
2022-03-19 20:18:44 +0100

    querySelector

---
+++ Curso JS Moderno/13-DOM/js/04-scripts.js
@@ -0,0 +1,23 @@
+//querySelector solo devuelve el primero
+const card = document.querySelector('.card');
+console.log(card);
+
+//sino existe devuelve null
+const patata = document.querySelector('.patata');
+console.log(patata);
+
+//podemos tener selectores expecificos como en css
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+//seleccionar el sgundo card de hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+//seleccionar por id
+const formulario = document.querySelector('#formulario');
+console.log(formulario);
+
+//seleccionar elementos html por su etiqueta
+const navegacion = document.querySelector('nav');
+console.log(navegacion);
