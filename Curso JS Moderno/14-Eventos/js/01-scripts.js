b1939e6fc7982f47015f01033f3ca824a60d390c
cparradoc <cparradoc@minsait.com>
2022-03-20 10:18:39 +0100

    domcontentloaded event

---
+++ Curso JS Moderno/14-Eventos/js/01-scripts.js
@@ -0,0 +1,9 @@
+console.log(1);
+
+//Este evento espera a que todo el documento esté listo
+document.addEventListener('DOMContentLoaded', () => { 
+    console.log('Documento listo');
+});
+
+console.log(3);
+
