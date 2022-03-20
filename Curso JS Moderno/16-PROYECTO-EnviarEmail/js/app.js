d20ba670dbcd3c98d41a88f3fec1d7be822ae89d
cparradoc <cparradoc@minsait.com>
2022-03-20 19:02:00 +0100

    only one validation error message

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,5 +1,7 @@
 //Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
+
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
@@ -25,5 +27,20 @@ function validarFormulario(e) {
         console.log("escribe anda");
     }else{
         e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
     }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100',
+     'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+     const errores = document.querySelectorAll('.error');
+     if(errores.length === 0){
+        formulario.appendChild(mensajeError);
+     }
+
 }
