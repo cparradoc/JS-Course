4bcd77be5518b5b82b7fc2a1d67001e2ea810508
cparradoc <cparradoc@minsait.com>
2022-03-21 10:55:32 +0100

    form validation

--- Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -16,5 +16,23 @@ function eventListeners() {
 function agregarTweet(e) {
     e.preventDefault();
 
+    const tweet = document.querySelector('#tweet').value;
+
+    if(tweet == '') {
+        mostrarError('Un mensaje no puede ir vacío');
+        return;
+    }
     
+}
+
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    const contenedor = document.querySelector('#contenido');
+    contenedor.appendChild(mensajeError);
+    setTimeout( () => {
+        mensajeError.remove();
+    }, 3000);
 }
