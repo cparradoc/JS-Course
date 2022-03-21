1fd1c4cb886d7777de9894ac5ac0120115f10813
cparradoc <cparradoc@minsait.com>
2022-03-21 18:38:02 +0100

    expenses on html

--- Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
+++ Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -1,6 +1,6 @@
 //Variables
 const formulario = document.querySelector('#agregar-gasto');
-const gastoListado = document.querySelector('#gasto ul');
+const gastoListado = document.querySelector('#gastos ul');
 
 
 
@@ -51,6 +51,36 @@ class UI {
             divAlerta.remove();
         }, 3000);
     }
+
+    agregarGastoListado(gastos) {
+
+        this.limpiarHTML();
+
+        gastos.forEach( gasto => {
+            const {cantidad, nombre, id} = gasto;
+
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id;
+
+            nuevoGasto.innerHTML = `${nombre}<span class="bas¡dge badge-primary badge-pill" ${cantidad} </span>`;
+
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = 'Borrar &times';
+
+            nuevoGasto.appendChild(btnBorrar);
+
+            gastoListado.appendChild(nuevoGasto);
+
+        });
+    }
+
+    limpiarHTML() {
+        while(gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
+    }
 }
 
 //Instanciar
@@ -83,7 +113,7 @@ function agregarGasto(e) {
         ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
         return;
     } else if (cantidad <= 0 || isNaN(cantidad)){
-        ui.imprimirAlerta('Cantidad no válida');
+        ui.imprimirAlerta('Cantidad no válida', 'error');
         return;
     }
 
@@ -93,6 +123,9 @@ function agregarGasto(e) {
     presupuesto.nuevoGasto(gasto);
 
     ui.imprimirAlerta('Gasto agregado correctamente');
+
+    const {gastos} = presupuesto;
+    ui.agregarGastoListado(gastos);
 
     formulario.reset();
 }
