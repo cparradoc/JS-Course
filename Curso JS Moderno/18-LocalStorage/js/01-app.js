27d78ef32c76084c923414fdf4fafd4ccfaa0e07
cparradoc <cparradoc@minsait.com>
2022-03-21 10:30:14 +0100

    localstorage examples

--- Curso JS Moderno/18-LocalStorage/js/01-app.js
+++ Curso JS Moderno/18-LocalStorage/js/01-app.js
@@ -1,1 +1,16 @@
+//Solo almacena strings
 localStorage.setItem('nombre', 'Juan');
+
+sessionStorage.setItem('nombre', 'Juan');
+
+const producto = {
+    nombre: "Monitor 24 pulgasas",
+    precio: 200
+}
+
+const productoString = JSON.stringify(producto);
+localStorage.setItem('producto', productoString);
+
+const meses = ['Enero', 'Febrero', 'Marzo'];
+const mesesString = JSON.stringify(meses);
+localStorage.setItem('meses', mesesString);
