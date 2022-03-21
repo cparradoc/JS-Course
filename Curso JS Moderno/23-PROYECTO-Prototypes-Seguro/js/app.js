a64227c98dfd0b41f1aa93e2c37dd62666c38766
cparradoc <cparradoc@minsait.com>
2022-03-21 12:56:51 +0100

    form validation

--- Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
+++ Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -28,3 +28,29 @@
  document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); //Llena el select con los años
  });
+
+
+ eventListeners();
+ function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+ }
+
+ function cotizarSeguro(e) {
+     e.preventDefault();
+
+     //Leer la marca seleccionada
+     const marca = document.querySelector('#marca').value;
+
+
+     //Leer el año seleccionado
+     const year = document.querySelector('#year').value;
+
+
+     //Leer el tipo de cobertura
+     const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+     if(marca === '' || year === '' || tipo === '') {
+         console.log('no pasó la validación');
+     }
+ }
