44eb41d5f7e57ddfe58415963d780f1b0a9d3dda
cparradoc <cparradoc@minsait.com>
2022-03-21 21:12:06 +0100

    delete dates

--- Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -19,6 +19,10 @@ class Citas {
     agregarCita(cita) {
         this.citas = [...this.citas, cita];
     }
+
+    eliminarCita(id) {
+        this.citas = this.citas.filter( cita => cita.id !== id);
+    }
 }
 
 class UI {
@@ -88,7 +92,7 @@ class UI {
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('btn', 'btn-danger','mr-2');
             btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
-
+            btnEliminar.onclick = () => eliminarCita(id);
 
             //Agregar los parrafos al divCita
             divCita.appendChild(mascotaParrafo);
@@ -182,4 +186,13 @@ function reiniciarObjeto() {
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+}
+
+function eliminarCita(id) {
+    //Eliminar citas
+    administrarCitas.eliminarCita(id);
+    //Muestre un mensaje
+    ui.imprimirAlerta('La alerta se eliminó correctamente');
+    //Imprimir las citas
+    ui.imprimirCitas(administrarCitas);
 }
