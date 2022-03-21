13498768fb980e5817f8262ba170fa7fd261ba16
cparradoc <cparradoc@minsait.com>
2022-03-21 14:31:52 +0100

    class functions and static methods

--- Curso JS Moderno/24-POO/js/01-app.js
+++ Curso JS Moderno/24-POO/js/01-app.js
@@ -4,10 +4,20 @@ class Cliente {
         this.nombre = nombre;
         this.saldo = saldo;
     }
+
+    mostrarInformacion() {
+        return `Cliente ${this.nombre}`;
+    }
+
+    static bienvenida() {
+        return `Bienvenido al cajero`;
+    }
 }
 
 const juan = new Cliente('juan', 400);
-
+console.log(juan.mostrarInformacion());
+console.log(Cliente.bienvenida());
+//console.log(juan.bienvenida()); da error, porque se llama directamente desde la clase
 
 //Class expression
 const Cliente2 = class {
@@ -15,6 +25,11 @@ const Cliente2 = class {
         this.nombre = nombre;
         this.saldo = saldo;
     }
+
+    mostrarInformacion() {
+        return `Cliente ${this.nombre}`;
+    }
 }
 
 const juan2 = new Cliente2('juan', 400);
+console.log(juan2.mostrarInformacion());
