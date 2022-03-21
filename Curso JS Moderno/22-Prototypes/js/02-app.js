6bb0b90ce2a3709f52450966db101c832da71268
cparradoc <cparradoc@minsait.com>
2022-03-21 12:04:41 +0100

    prototype problems

---
+++ Curso JS Moderno/22-Prototypes/js/02-app.js
@@ -0,0 +1,30 @@
+function Cliente(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+}
+
+const juan = new Cliente('Juan', 500);
+
+console.log(juan);
+
+function formatearCliente(cliente) {
+    const {nombre, saldo} = cliente;
+    return `El ciente ${nombre} tiene un saldo de ${saldo}`;
+}
+
+function formatearEmpresa(empresa) {
+    const {nombre, saldo, categoria} = empresa;
+    return `El ciente ${nombre} tiene un saldo de ${saldo} y pertenece a la categoría ${categoria}`;
+}
+
+
+console.log(formatearCliente(juan));
+
+function Empresa(nombre, saldo, categoria) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+    this.categoria = categoria;
+}
+
+const CCJ = new Empresa('Codigo con Juan', 4000, 'Cursos en linea');
+console.log(formatearEmpresa(CCJ));
