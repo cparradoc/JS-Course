dbce938850d3a34950ce012046589593e6b1d9ab
cparradoc <cparradoc@minsait.com>
2022-03-21 11:47:48 +0100

    date object

---
+++ Curso JS Moderno/21-Fechas/js/01-app.js
@@ -0,0 +1,13 @@
+const diaHoy = new Date();
+
+let valor;
+
+valor = diaHoy;
+
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth(); //empieza en 0
+valor = diaHoy.getMinutes();
+valor = diaHoy.getTime(); //tiempo desde el 1 de enero de 1970
+
+
+console.log(valor);
