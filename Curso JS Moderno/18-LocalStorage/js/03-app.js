ead110046d953eb3183816544f9bab7553374425
cparradoc <cparradoc@minsait.com>
2022-03-21 10:39:44 +0100

    clear localstorage

--- Curso JS Moderno/18-LocalStorage/js/03-app.js
+++ Curso JS Moderno/18-LocalStorage/js/03-app.js
@@ -4,3 +4,7 @@ localStorage.removeItem('nombre');
 const mesesArray = JSON.parse(localStorage.getItem('meses'));
 mesesArray.push('Nuevo mes');
 localStorage.setItem('meses', JSON.stringify(mesesArray));
+
+
+
+localStorage.clear();
