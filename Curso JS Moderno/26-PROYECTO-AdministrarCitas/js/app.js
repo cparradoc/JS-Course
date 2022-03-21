b7cebbc6465d5c1ad252d8b088c5759c7c5a6952
cparradoc <cparradoc@minsait.com>
2022-03-21 21:07:14 +0100

    delete button

--- Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -84,6 +84,11 @@ class UI {
             <span class="font-weight-bolder">Síntomas: </span> ${telefono}
             `;
 
+            //Boton eliminar cita
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger','mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
+
 
             //Agregar los parrafos al divCita
             divCita.appendChild(mascotaParrafo);
@@ -92,6 +97,7 @@ class UI {
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomaParrafo);
+            divCita.appendChild(btnEliminar);
 
             //Agregar las citas al HTML
             contenedor.appendChild(divCita);
