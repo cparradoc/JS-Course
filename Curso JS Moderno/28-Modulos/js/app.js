9b100ee4973fe68ea8100de463d3f2ee2f653a0a
cparradoc <cparradoc@minsait.com>
2022-03-22 11:40:17 +0100

    export classes

--- Curso JS Moderno/28-Modulos/js/app.js
+++ Curso JS Moderno/28-Modulos/js/app.js
@@ -1,6 +1,11 @@
-import { nombre, ahorro, mostrarInformacion, tieneSaldo } from "./cliente.js";
+import { nombre, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente.js";
 
 console.log(nombre);
 console.log(ahorro);
 console.log(mostrarInformacion(nombre, ahorro));
 tieneSaldo(ahorro);
+
+const cliente = new Cliente(nombre, ahorro);
+
+console.log(cliente);
+console.log(cliente.mostrarInformacion());
