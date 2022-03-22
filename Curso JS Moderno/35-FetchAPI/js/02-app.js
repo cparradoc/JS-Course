64846000a379bbe76d52475a9e66df852a5bf9fa
cparradoc <cparradoc@minsait.com>
2022-03-22 19:22:46 +0100

    json fetch

---
+++ Curso JS Moderno/35-FetchAPI/js/02-app.js
@@ -0,0 +1,30 @@
+const cargarJSONBtn = document.querySelector('#cargarJSON');
+cargarJSONBtn.addEventListener('click', obtenerDatos);
+
+
+function obtenerDatos() {
+
+    const url = 'data/empleado.json';
+
+    fetch(url)
+        .then( respuesta => {
+            return respuesta.json();
+        })
+        .then( datos => {
+            mostrarHTML(datos);
+        })
+        .catch (error => {
+            console.log(error);
+        });
+        
+}
+
+function mostrarHTML({empresa, id, nombre, trabajo}) {
+    const contenido = document.querySelector('.contenido');
+    contenido.innerHTML = `
+    <p>Empleado: ${nombre}</p>
+    <p>ID: ${id}</p>
+    <p>Empresa: ${empresa}</p>
+    <p>Trabajo: ${trabajo}</p>
+    `
+}
