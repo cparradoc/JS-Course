6c2ac321bf3173e493fc1a9d96ba61325f6065d2
cparradoc <cparradoc@minsait.com>
2022-03-22 11:16:04 +0100

    export-import functions

--- Curso JS Moderno/28-Modulos/js/app.js
+++ Curso JS Moderno/28-Modulos/js/app.js
@@ -1,3 +1,6 @@
-import { nombre } from "./cliente.js";
+import { nombre, ahorro, mostrarInformacion, tieneSaldo } from "./cliente.js";
 
 console.log(nombre);
+console.log(ahorro);
+console.log(mostrarInformacion(nombre, ahorro));
+tieneSaldo(ahorro);
--- Curso JS Moderno/28-Modulos/js/cliente.js
+++ Curso JS Moderno/28-Modulos/js/cliente.js
@@ -5,4 +5,17 @@
 })();*/
 
 export const nombre = 'Juan';
+export const ahorro = 200;
 
+
+export function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
+}
+
+export function tieneSaldo(ahorro) {
+    if(ahorro > 0) {
+        console.log("Tiene saldo");
+    } else {
+        console.log("No tiene saldo");
+    }
+}
