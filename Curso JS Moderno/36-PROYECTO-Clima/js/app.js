08ab186aff74b27cefa0ba9ee9292173daa418ba
cparradoc <cparradoc@minsait.com>
2022-03-22 20:30:32 +0100

    show actual temperature

--- Curso JS Moderno/36-PROYECTO-Clima/js/app.js
+++ Curso JS Moderno/36-PROYECTO-Clima/js/app.js
@@ -46,11 +46,38 @@ function consultarAPI(ciudad, pais) {
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
+            limpiarHTML();
             if(datos.cod === '404') {
                 mostrarError('Ciudad no encontrada');
-            } else {
-                console.log(datos);
+                return;
             }
+            mostrarClima(datos);
         });
 
+}
+
+function mostrarClima(datos) {
+    const { main: {temp, temp_max, temp_min}} = datos;
+
+    const centigrados = parseFloat(temp);
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold','text-6xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(actual);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+function kelvinACentigrados(grados) {
+    return parseFloat(grados - 273.15);
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
