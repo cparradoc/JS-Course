3beeba9503177a4241ee1df14b0a8fa9fdbf8140
cparradoc <cparradoc@minsait.com>
2022-03-23 13:48:00 +0100

    creating form

---
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,22 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+}
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    const camposVacios = [mesa, hora].some(campo => campo === '');
+    if(camposVacios) {
+
+    } else {
+        mostrarAlerta();
+    }
+
+
+}
