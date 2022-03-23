e38524ab45299f8486c4e763a6079521ac18932f
cparradoc <cparradoc@minsait.com>
2022-03-23 10:07:50 +0100

    validate search song form

---
+++ Curso JS Moderno/37-PROYECTO-BuscarCanciones/js/app.js
@@ -0,0 +1,28 @@
+import * as UI from './interfaz.js';
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion);
+
+
+
+function buscarCancion(e) {
+    e.preventDefault();
+
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if(artista === '' || cancion === '') {
+        //El usuario dejó al menos un campo vacío
+        UI.divmensajes.textContent = 'Error, todos los campos son obligatorios';
+        UI.divmensajes.classList.add('error');
+        setTimeout(() => {
+            UI.divmensajes.textContent = ''
+            UI.divmensajes.classList.remove('error');
+        }, 3000);
+        
+        return;
+    }
+
+
+    //Consultar nuestra API
+}
