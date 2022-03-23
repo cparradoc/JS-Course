de0164608242e670b5ca7940ab3fbf38f0997da7
cparradoc <cparradoc@minsait.com>
2022-03-23 17:52:36 +0100

    async await

---
+++ Curso JS Moderno/41-AsyncAwait/js/02-app.js
@@ -0,0 +1,29 @@
+function descargarClientes() {
+    return new Promise((resolve, reject) => {
+        const error = false;
+
+        setTimeout(() => {
+            if(!error) {
+                resolve('Listado de clientes correcto');
+            } else{
+                reject('Error en la conexión');
+            }
+        }, 3000);
+    });
+}
+
+//Async await
+
+async function ejecutar() {
+    try {
+        console.log(1 + 1);
+        //await detiene la ejecución del código hasta que se resuelve el promise
+        const respuesta = await descargarClientes(); 
+
+        console.log(2 + 2);
+        console.log(respuesta);
+    } catch(error) {
+        console.log(error);
+    }
+}
+ejecutar();
