a24c0b4432a2be177a7c696366fd95fe4de2d058
cparradoc <cparradoc@minsait.com>
2022-03-23 16:59:20 +0100

    tip form

--- Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -147,7 +147,7 @@ function agregarPlatillo(producto) {
 function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
     const resumen = document.createElement('div');
-    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
+    resumen.classList.add('col-md-6', 'card', 'py-2', 'px-3', 'shadow');
 
     //Información de la mesa
     const mesa = document.createElement('p');
@@ -241,12 +241,15 @@ function actualizarResumen() {
         grupo.appendChild(lista);
     });
 
+    resumen.appendChild(heading);
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
-    resumen.appendChild(heading);
     resumen.appendChild(grupo);
     
     contenido.appendChild(resumen);
+
+    //Mostrar formulario de propinas
+    formularioPropinas();
 
 }
 
@@ -289,4 +292,24 @@ function mensajePedidoVacio() {
     texto.textContent = 'Añade los elementos del pedido';
 
     contenido.appendChild(texto);
+}
+
+function formularioPropinas() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const formulario = document.createElement('div');
+    formulario.classList.add('col-md-6', 'formulario');
+
+    const divFormulario = document.createElement('div');
+    divFormulario.classList.add('card', 'py-2', 'px-3', 'shadow');
+
+
+    const heading = document.createElement('h3');
+    heading.classList.add('my-4', 'text-center');
+    heading.textContent = 'Propina';
+
+
+    divFormulario.appendChild(heading);
+    formulario.appendChild(divFormulario);
+    contenido.appendChild(formulario);
 }
