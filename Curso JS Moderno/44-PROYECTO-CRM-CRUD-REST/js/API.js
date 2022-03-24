5cb825af9285db021c7f6c6f578a9c28915bd937
cparradoc <cparradoc@minsait.com>
2022-03-24 12:24:37 +0100

    delete clients

--- Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -26,3 +26,14 @@ export const obtenerClientes = async () => {
         console.log(error);
     }
 }
+
+//Eliminar cliente
+export const eliminarCliente = async id => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        });
+    } catch(error) {
+        console.log(error);
+    }
+}
