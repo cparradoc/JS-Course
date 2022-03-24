fd48591fee0b8bad64564d8ac7c882f8c33925de
cparradoc <cparradoc@minsait.com>
2022-03-24 13:02:40 +0100

    edit client part 2

--- Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
+++ Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
@@ -1,4 +1,5 @@
-import {obtenerCliente} from './API.js'
+import {editarCliente, obtenerCliente} from './API.js'
+import {mostrarAlerta, validar} from './funciones.js'
 
 (function() {
 
@@ -18,6 +19,10 @@ import {obtenerCliente} from './API.js'
         const cliente = await obtenerCliente(idCliente);
         
         mostrarCliente(cliente);
+
+        //Submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
     });
 
     function mostrarCliente(cliente) {
@@ -27,5 +32,26 @@ import {obtenerCliente} from './API.js'
         emailInput.value = email;
         empresaInput.value = empresa;
         idInput.value = id;
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id:parseInt(idInput.value) 
+        }
+
+        if (validar(cliente)){
+            //Mostrar mensaje error
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+        //Reescribe el objeto
+        editarCliente(cliente);
     }
 })();
