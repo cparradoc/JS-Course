c7149f62568834f0849a0ef5712516e358f2ddd3
cparradoc <cparradoc@minsait.com>
2022-03-25 19:20:34 +0100

    show result

--- Curso JS Moderno/prestamos/src/componentes/Resultado.js
+++ Curso JS Moderno/prestamos/src/componentes/Resultado.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Resultado = () => {
+const Resultado = ({cantidad, plazo, total}) => {
     return (
-        <p></p>
+        <div className='u-full-width resultado'>
+            <h2>Resumen</h2>
+            <p>La cantidad solicitada es: {cantidad}$</p>
+            <p>A pagar en: {plazo} meses</p>
+            <p>Su pago mensual es: {(cantidad/plazo).toFixed(2)} $/mes</p>
+            <p>Total a pagar: {(total).toFixed(2)} $</p>
+        </div>
     );
 }
 
