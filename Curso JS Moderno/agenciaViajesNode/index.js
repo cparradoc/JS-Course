40a57803bdbdfa7793517f964560245f72ef7d7b
cparradoc <cparradoc@minsait.com>
2022-03-25 21:01:20 +0100

    simple routing from index

--- Curso JS Moderno/agenciaViajesNode/index.js
+++ Curso JS Moderno/agenciaViajesNode/index.js
@@ -1,10 +1,22 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 //Definir puerto
 const port = process.env.PORT || 4000;
 
-app.listen(() =>{
+app.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
+    res.send('Inicio');
+});
+
+app.get('/nosotros', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
+    res.send('Contacto');
+});
+
+app.listen(port, () =>{
     console.log(`El servidor está funcionando en el puerto ${port}`);
 })
