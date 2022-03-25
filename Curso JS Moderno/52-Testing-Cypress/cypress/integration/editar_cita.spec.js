7f443fe752ec15bbe7636870eed25ce34b7feb0d
cparradoc <cparradoc@minsait.com>
2022-03-25 16:33:33 +0100

    edit dates

--- Curso JS Moderno/52-Testing-Cypress/cypress/integration/editar_cita.spec.js
+++ Curso JS Moderno/52-Testing-Cypress/cypress/integration/editar_cita.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-describe('Llena los campos para una nueva cita', () =>{
+describe('Llena los campos para una nueva cita y la edita', () =>{
     it('Campos nueva cita', () => {
 
         cy.visit('/index.html');
 
         cy.get('[data-cy="mascota-input"')
-        .type('Hook');
+        .type('Nuevo Hook');
 
         cy.get('[data-cy="propietario-input"')
         .type('Carlos');
@@ -35,4 +35,25 @@ describe('Llena los campos para una nueva cita', () =>{
         cy.get('[data-cy="alerta"]')
         .should('have.class', 'alert-success');
     });
+
+    it('Edita la cita', () => {
+        cy.get('[data-cy="btn-editar"]')
+        .click();
+
+        cy.get('[data-cy="mascota-input"')
+        .clear()
+        .type('NUEVO Hook');
+
+        cy.get('[data-cy="submit-cita"')
+        .click();
+
+        cy.get('[data-cy="alerta"]').invoke('text')
+        .should('equal', 'Guardado Correctamente');
+
+        cy.get('[data-cy="alerta"]')
+        .should('have.class', 'alert-success');
+
+    });
+
+
 });
