676d956eba3cc5ae2e64496626e385acf808d51f
cparradoc <cparradoc@minsait.com>
2022-03-25 14:04:03 +0100

    cypress attributes

--- Curso JS Moderno/52-Testing-Cypress/cypress/integration/primer_test.spec.js
+++ Curso JS Moderno/52-Testing-Cypress/cypress/integration/primer_test.spec.js
@@ -5,8 +5,21 @@ describe('Carga la página principal', () =>{
 
         cy.visit('http://127.0.0.1:5500/52-Testing-Cypress/index.html');
 
-        cy.contains('h1', 'Administrador de Pacientes de Veterinaria');
+        //Verificar el elemento y su texto
+        cy.contains('[data-cy="titulo-proyecto"]', 'Administrador de Pacientes de Veterinaria');
 
-        cy.get('h1').should('exist');
+        //Verificar que exista
+        cy.get('[data-cy="titulo-proyecto"]').should('exist');
+
+        //Verificar que exista el elemento y contenga un texto
+        cy.get('[data-cy="titulo-proyecto"]').invoke('text')
+            .should('equal', 'Administrador de Pacientes de Veterinaria');
+
+        //Verificar el texto de las citas
+        cy.get('[data-cy=citas-heading]').invoke('text')
+            .should('equal', 'No hay Citas, comienza creando una');
+
+        cy.get('[data-cy=citas-heading]').invoke('text')
+            .should('not.equal', 'Juan Pablo');
     });
 });
