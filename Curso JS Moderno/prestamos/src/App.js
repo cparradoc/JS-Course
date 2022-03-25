66daf9dfbc27fc1326cf11e1ee91dcedbcf7dc23
cparradoc <cparradoc@minsait.com>
2022-03-25 19:15:15 +0100

    conditional charge

--- Curso JS Moderno/prestamos/src/App.js
+++ Curso JS Moderno/prestamos/src/App.js
@@ -1,6 +1,8 @@
 import React, {Fragment, useState} from 'react';
 import Header from './componentes/Header'
 import Formulario from './componentes/Formulario'
+import Mensaje from './componentes/Mensaje'
+import Resultado from './componentes/Resultado'
 import './App.css';
 
 function App() {
@@ -9,6 +11,13 @@ function App() {
     const [cantidad, guardarCantidad] = useState(0);
     const [plazo, guardarPlazo] = useState('');
     const [total, guardarTotal] = useState(0);
+
+    let componente;
+    if(total === 0) {
+      componente = <Mensaje/>
+    } else {
+      componente = <Resultado/>
+    }
 
   return (
     <Fragment>
@@ -26,7 +35,10 @@ function App() {
           total = {total}
           guardarTotal = {guardarTotal}
         />
-        <p>Total a pagar {total} $</p>
+        <div className='mensajes'>
+          {componente}
+        </div>
+
       </div>
     </Fragment>
   );
---
+++ Curso JS Moderno/prestamos/src/componentes/Resultado.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Resultado = () => {
+    return (
+        <p></p>
+    );
+}
+
+export default Resultado;
