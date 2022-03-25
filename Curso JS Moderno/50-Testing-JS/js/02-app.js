e8167ba90d0395ba214eea837673893ef052e4f8
cparradoc <cparradoc@minsait.com>
2022-03-25 11:22:56 +0100

    expected to be

---
+++ Curso JS Moderno/50-Testing-JS/js/02-app.js
@@ -0,0 +1,39 @@
+//Probar 2 valores
+
+function suma(a,b) {
+    return a + b;
+}
+
+function restar(a,b) {
+    return a - b;
+}
+
+let resultado = suma(2, 2);
+let esperado = 3;
+
+expected(esperado).toBe(resultado);
+
+resultado = restar(10, 5);
+esperado = 5;
+
+expected(esperado).toBe(resultado);
+
+
+function expected(esperado) {
+    return {
+        toBe(resultado) {
+            if(resultado !== esperado) {
+                console.error(`El ${resultado} es diferente a lo esperado, la prueba no pasó`);
+            } else {
+                console.log('La prueba pasó correctamente');
+            }
+        },
+        toEqual(resultado) {
+            if(resultado !== esperado) {
+                console.error(`El ${resultado} no es igual a lo esperado, la prueba no pasó`);
+            } else {
+                console.log('La prueba pasó correctamente');
+            }
+        }
+    }
+}
