0d12632ea31e7ac3561cb0467433b73c5201e9b3
cparradoc <cparradoc@minsait.com>
2022-03-26 15:48:43 +0100

    backend first steps

--- Curso JS Moderno/MERN/backend/index.js
+++ Curso JS Moderno/MERN/backend/index.js
@@ -1,2 +1,12 @@
 import express from "express";
 
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send('Hola mundo');
+})
+
+app.listen(4000, () => {
+    console.log('Servidor funcionando en el puerto 4000');
+});
+
