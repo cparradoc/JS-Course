9f9361818b34a9f7c39c8f4df3c4a67cc89dc785
cparradoc <cparradoc@minsait.com>
2022-03-26 22:49:04 +0100

    protected routes from middleware

---
+++ Curso JS Moderno/MERN/backend/middleware/authMiddleware.js
@@ -0,0 +1,7 @@
+const checkAuth = (req, res, next) => {
+
+        console.log('desde mi middleware');
+        next();
+};
+
+export default checkAuth;
--- Curso JS Moderno/MERN/backend/routes/veterinarioRoutes.js
+++ Curso JS Moderno/MERN/backend/routes/veterinarioRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { registrar, perfil, confirmar, autenticar} from "../controllers/veterinarioController.js";
 const router = express.Router();
+import checkAuth from '../middleware/authMiddleware.js';
 
 
 router.post('/', registrar);
-router.get('/perfil', perfil);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
 
+router.get('/perfil', checkAuth, perfil);
 
 export default router;
