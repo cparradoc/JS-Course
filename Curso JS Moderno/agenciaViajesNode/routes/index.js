f7cac4c2a1521e0869809e18d288a1de76000b54
cparradoc <cparradoc@minsait.com>
2022-03-26 10:51:22 +0100

    variables on pug views

--- Curso JS Moderno/agenciaViajesNode/routes/index.js
+++ Curso JS Moderno/agenciaViajesNode/routes/index.js
@@ -3,11 +3,16 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
-    res.send('Inicio');
+    res.render('inicio');
 });
 
 router.get('/nosotros', (req, res) => { // req - lo que enviamos, res - lo que express nos responde
-    res.render('nosotros');
+
+    const viajes = 'Viaje a Alemania';
+
+    res.render('nosotros', {
+        viajes
+    });
 });
 
 
