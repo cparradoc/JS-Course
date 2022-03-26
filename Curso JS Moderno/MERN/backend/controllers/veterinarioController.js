1a98c8aa7dccfae8ce8a67dce5d4275cd93b5e9a
cparradoc <cparradoc@minsait.com>
2022-03-26 22:09:42 +0100

    generate jwt

--- Curso JS Moderno/MERN/backend/controllers/veterinarioController.js
+++ Curso JS Moderno/MERN/backend/controllers/veterinarioController.js
@@ -1,4 +1,5 @@
 import Veterinario from "../models/Veterinario.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
    const {email} = req.body;
@@ -66,7 +67,7 @@ const autenticar = async (req, res) => {
 
     //Revisar el password
     if(await usuario.comprobarPassword(password)) {
-        console.log('Pasword correcto');
+        res.json({token: generarJWT(usuario.id)});
     }else{ 
         const error = new Error('Password incorrecto');
         return res.status(403).json({msg: error.message});
