134147bc1fb3d623e0314338bcdc75276e374283
cparradoc <cparradoc@minsait.com>
2022-03-26 23:40:43 +0100

    check valid token

--- Curso JS Moderno/MERN/backend/routes/veterinarioRoutes.js
+++ Curso JS Moderno/MERN/backend/routes/veterinarioRoutes.js
@@ -10,7 +10,7 @@ router.post('/', registrar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
 router.post('/olvide-password', olvidePassword);
-router.get('olvide-password/:token', comprobarToken);
+router.get('/olvide-password/:token', comprobarToken);
 router.post('/olvide-password/:token', nuevoPassword);
 
 //Área privada
