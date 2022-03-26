d15087213c1b01a504493fb250647467b017487b
cparradoc <cparradoc@minsait.com>
2022-03-26 12:24:44 +0100

    controller

---
+++ Curso JS Moderno/agenciaViajesNode/controllers/paginasController.js
@@ -0,0 +1,30 @@
+const paginaInicio = (req, res) => {
+    res.render('inicio', {
+    pagina: 'Inicio'
+    });
+};
+
+const paginaNosotros = (req, res) => { // req - lo que enviamos, res - lo que express nos responde
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+};
+
+const paginaViajes = (req, res) => {
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+};
+
+const paginaTestimoniales = (req, res) => {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
+};
+
+export {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales
+}
