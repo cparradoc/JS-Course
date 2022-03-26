96a15ed5384f5430406cbdb6cbf6a6c388b27cb1
cparradoc <cparradoc@minsait.com>
2022-03-26 17:57:08 +0100

    validate users by token

--- Curso JS Moderno/MERN/backend/controllers/veterinarioController.js
+++ Curso JS Moderno/MERN/backend/controllers/veterinarioController.js
@@ -26,7 +26,29 @@ const perfil =(req, res) => {
     res.json({msg: 'mostrando perfil'});
 }
 
+const confirmar = async(req, res) => {
+    const {token} = req.params;
+    const usuarioConfirmar = await Veterinario.findOne({token});
+
+    if(!usuarioConfirmar) {
+        const error = new Error('Token no válido');
+        return res.status(404).json({msg: error.message});
+
+    } 
+    
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmar = true;
+        await usuarioConfirmar.save();
+        res.json({msg: 'Usuario confirmado correctamente'});
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
 export {
     registrar,
-    perfil
+    perfil,
+    confirmar
 };
