a9eb1a3f12ff98965fc91e43bfad3389ffdea75b
cparradoc <cparradoc@minsait.com>
2022-03-26 14:01:08 +0100

    validate form

--- Curso JS Moderno/agenciaViajesNode/controllers/testimonialController.js
+++ Curso JS Moderno/agenciaViajesNode/controllers/testimonialController.js
@@ -9,11 +9,27 @@ const guardarTestimonial = (req, res) => {
     if(nombre.trim() === '') {
         errores.push({mensaje : 'El nombre está vacío'});
     }
+
     if(correo.trim() === '') {
-        console.push({mensaje : 'El correo está vacío'});
+        errores.push({mensaje : 'El correo está vacío'});
     }
-    if(nombre.trim() === '') {
-        mensaje.push({mensaje : 'El mensaje está vacío'});
+
+    if(mensaje.trim() === '') {
+        errores.push({mensaje : 'El mensaje está vacío'});
+    }
+
+    if(errores.length > 0) {
+        //Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        });
+    } else {
+        //Almacenarlo en la base de datos
+        
     }
     
 }
