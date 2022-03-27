39960357cca2fe175ff29cbafb7a07ff5871e2e8
cparradoc <cparradoc@minsait.com>
2022-03-27 13:20:13 +0200

    save patient with vet id

--- Curso JS Moderno/MERN/backend/routes/pacienteRoutes.js
+++ Curso JS Moderno/MERN/backend/routes/pacienteRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
 import {obtenerPacientes, agregarPaciente} from '../controllers/pacienteController.js'
+import checkAuth from '../middleware/authMiddleware.js'
 
-router.route('/').post(agregarPaciente).get(obtenerPacientes);
+router.route('/').post(checkAuth, agregarPaciente).get(obtenerPacientes);
 
 export default router;
