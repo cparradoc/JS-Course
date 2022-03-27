7dc58b009ac8ac555902ab9f062acb7887af82ee
cparradoc <cparradoc@minsait.com>
2022-03-27 19:20:27 +0200

    update pet info

--- Curso JS Moderno/MERN/backend/controllers/pacienteController.js
+++ Curso JS Moderno/MERN/backend/controllers/pacienteController.js
@@ -19,18 +19,38 @@ const obtenerPacientes = async (req, res) => {
 const obtenerPaciente = async (req, res) => {
     const { id } = req.params;
     const paciente = await Paciente.findById(id);
-    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
-        res.json({msg: "Acción no válida"});
-        return;
+    if (!paciente) {
+        res.status(404).json({msg: "Usuario no encontrado"});
     }
-    if(paciente){
-        res.json(paciente);
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida"});
     }
 
+    res.json(paciente);
 }
 
 const actualizarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+    if (!paciente) {
+        res.status(404).json({msg: "Usuario no encontrado"});
+    }
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida"});
+    }
 
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+    
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const eliminarPaciente = async (req, res) => {
