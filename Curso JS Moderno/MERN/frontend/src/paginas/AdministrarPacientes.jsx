35ae6155e1982240b50ee9c88d346c15418ec48a
cparradoc <cparradoc@minsait.com>
2022-03-28 20:05:54 +0200

    client area created

--- Curso JS Moderno/MERN/frontend/src/paginas/AdministrarPacientes.jsx
+++ Curso JS Moderno/MERN/frontend/src/paginas/AdministrarPacientes.jsx
@@ -1,7 +1,16 @@
+import Formulario from "../components/Formulario";
+import ListadoPacientes from "../components/ListadoPacientes";
 
 const AdministrarPacientes = () => {
-    return(
-        <h1>Desde administrar pacientes</h1>
+    return (
+        <div className="flex flex-col md:flex-row">
+            <div className="md:w-1/2 lg:w-2/5 ">
+                <Formulario />
+            </div>
+            <div className="md:w-1/2 lg:w-3/5">
+                <ListadoPacientes />
+            </div>
+        </div>
     );
 };
 
