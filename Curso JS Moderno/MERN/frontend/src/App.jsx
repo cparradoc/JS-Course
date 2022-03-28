4cc13b0ab15fa35c4996cc65b1eb9d761e9fa1da
cparradoc <cparradoc@minsait.com>
2022-03-28 09:28:48 +0200

    create public area pages

--- Curso JS Moderno/MERN/frontend/src/App.jsx
+++ Curso JS Moderno/MERN/frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { Router } from 'react-router-dom';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AuthLayout from './layout/AuthLayout';
 import Login from './paginas/Login';
+import Registrar from './paginas/Registrar';
+import OlvidePassword from './paginas/OlvidePassword';
+import ConfirmarCuenta from './paginas/ConfirmarCuenta';
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<AuthLayout />}>
           <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
         </Route>
       </Routes>
     </BrowserRouter>
