92d8a0734df51e89f628a0ebd2b901dfef295a4f
cparradoc <cparradoc@minsait.com>
2022-03-28 17:15:56 +0200

    create custom hook fo global context data

--- Curso JS Moderno/MERN/frontend/src/paginas/Login.jsx
+++ Curso JS Moderno/MERN/frontend/src/paginas/Login.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const Login = () => {
+
+
     return (
         <>
-            
             <div>
                 <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión u Administra tus {""}<span className="text-black">Pacientes</span></h1>
             </div>
