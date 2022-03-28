797025378d98c4c0e50f93b8c7ece3f133f2fea4
cparradoc <cparradoc@minsait.com>
2022-03-28 19:32:04 +0200

    sign out

--- Curso JS Moderno/MERN/frontend/src/components/Header.jsx
+++ Curso JS Moderno/MERN/frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const Header = () => {
+    const {cerrarSesion} = useAuth();
+
     return (
         <header className='py-10 bg-indigo-600'>
             <div className='container mx-auto flex flex-col lg:flex-row justify-between items-center'>
@@ -12,7 +15,8 @@ const Header = () => {
                     <Link to ="/admin" className='text-white text-sm uppercase font-bold'>Pacientes</Link>
                     <Link to ="/admin" className='text-white text-sm uppercase font-bold'>Perfil</Link>
 
-                    <button type="button" className='text-white text-sm uppercase font-bold'>Cerrar Sesión</button>
+                    <button type="button" onClick={cerrarSesion}
+                    className='text-white text-sm uppercase font-bold'>Cerrar Sesión</button>
                 </nav>
             </div>
 
