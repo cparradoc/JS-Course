cb34943cab4617701106a2dec5d4811a2fb2ac6a
cparradoc <cparradoc@minsait.com>
2022-03-28 10:45:39 +0200

    forgot my password page

--- Curso JS Moderno/MERN/frontend/src/paginas/Registrar.jsx
+++ Curso JS Moderno/MERN/frontend/src/paginas/Registrar.jsx
@@ -32,7 +32,7 @@ const Registrar = () => {
                         </label>
                         <input type="password" placeholder="Repite tu Password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
                     </div>
-                    <input type="submit" value="Iniciar Sesión" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
+                    <input type="submit" value="Crear Cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
                 </form>
                 <nav className="mt-10 lg:flex lg:justify-between">
                     <Link to="/" className="block text-center my-5 text-gray-500">¿Ya tienes una cuenta? Inicia Sesión</Link>
