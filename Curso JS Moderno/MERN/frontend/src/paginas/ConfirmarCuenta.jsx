4ca025305a7f4383f87216c3d3dead2dae883ac0
cparradoc <cparradoc@minsait.com>
2022-03-28 14:22:38 +0200

    confirm user account

--- Curso JS Moderno/MERN/frontend/src/paginas/ConfirmarCuenta.jsx
+++ Curso JS Moderno/MERN/frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,48 @@
+import { useParams, Link } from "react-router-dom";
+import {useEffect, useState} from 'react';
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 const ConfirmarCuenta = () => {
+
+    const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+    const [cargando, setCargando] = useState(true);
+    const [alerta, setAlerta] = useState({});
+
+    const params = useParams();
+    const {id} = params;
+
+    useEffect(() => {
+        const confirmarCuenta  = async () => {
+            try{
+                const url = `http://localhost:4000/api/veterinarios/confirmar/${id}`;
+                const {data} = await axios.get(url);
+
+                setCuentaConfirmada(true);
+                setAlerta({
+                    msg: data.msg
+                });
+            } catch (error) {
+                setAlerta({
+                    msg: error.response.data.msg,
+                    error: true
+                });
+            }
+        }
+        setCargando(false);
+        confirmarCuenta();
+    }, []);
+
     return (
         <>
-            <h1>ConfirmarCuenta</h1>
+            <div>
+                <h1 className="text-indigo-600 font-black text-6xl">Confirma tu Cuenta y comienza a Administrar tus{""}<span className="text-black"> Pacientes</span></h1>
+            </div>
+
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+                {!cargando && <Alerta alerta={alerta}/>}
+                {cuentaConfirmada && (<Link to="/" className="block text-center my-5 text-gray-500">Iniciar Sesión</Link>)}
+            </div>
         </>
     );
 };
