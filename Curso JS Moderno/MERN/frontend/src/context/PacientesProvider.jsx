36c9484dc82df2f9e17647cc4ee80c47ac11fc19
cparradoc <cparradoc@minsait.com>
2022-03-29 08:50:47 +0200

    create patients context

---
+++ Curso JS Moderno/MERN/frontend/src/context/PacientesProvider.jsx
@@ -0,0 +1,20 @@
+import {createContext, useEffect, useState} from 'react'
+import clienteAxios from '../config/axios'
+
+const PacientesContext = createContext();
+
+export const PacientesProvider = ({children}) => {
+
+    
+    return(
+        <PacientesContext.Provider
+            value={{
+
+            }}>
+            {children}
+        </PacientesContext.Provider>
+    )
+}
+
+
+export default PacientesContext;
