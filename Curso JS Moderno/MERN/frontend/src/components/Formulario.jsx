29bae69555ece5e7d495c358cdb4bae9cd970266
cparradoc <cparradoc@minsait.com>
2022-03-29 08:41:03 +0200

    typo with class, it is classname

--- Curso JS Moderno/MERN/frontend/src/components/Formulario.jsx
+++ Curso JS Moderno/MERN/frontend/src/components/Formulario.jsx
@@ -18,28 +18,28 @@ const Formulario = () => {
             <span className="text-indigo-600 font-bold">Administralos</span>
         </p>
         <form className="bg-white py-10 px-5 mb-10 lg:mb-0 shadow-md rounded-md">
-            <div class="mb-5">
+            <div className="mb-5">
                 <label htmlFor="nombre" className="text-gray-700 uppercase font-bold">Nombre Mascota</label>
                 <input id="nombre" type="text" placeholder="Nombre de la Mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={nombre} onChange={e => setNombre(e.target.value)} />
             </div>
-            <div class="mb-5">
+            <div className="mb-5">
                 <label htmlFor="propietario" className="text-gray-700 uppercase font-bold">Nombre   Propietario</label>
                 <input id="propietario" type="text" placeholder="Nombre del Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={propietario} onChange={e => setPropietario(e.target.value)}/>
             </div>
-            <div class="mb-5">
+            <div className="mb-5">
                 <label htmlFor="email" className="text-gray-700 uppercase font-bold">Email Propietario</label>
                 <input id="email" type="email" placeholder="Email Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={email} onChange={e => setEmail(e.target.value)}/>
             </div>
-            <div class="mb-5">
+            <div className="mb-5">
                 <label htmlFor="fecha" className="text-gray-700 uppercase font-bold">Fecha Alta</label>
                 <input id="fecha" type="date" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={fecha} onChange={e => setFecha(e.target.value)}/>
             </div>
-            <div class="mb-5">
+            <div className="mb-5">
                 <label htmlFor="sintomas" className="text-gray-700 uppercase font-bold">Síntomas</label>
                 <textarea id="sintomas" placeholder="Describe los síntomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" value={sintomas} onChange={e => setSintomas(e.target.value)}/>
             </div>
 
-            <input type="submot" value="Agregar Paciente" className="bg-indigo-600 w-full p-3 text-white text-center uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"/>
+            <input type="submit" value="Agregar Paciente" className="bg-indigo-600 w-full p-3 text-white text-center uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"/>
         </form>
         </>
     );
